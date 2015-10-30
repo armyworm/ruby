3300ef7572c125817927e2d788f61f3637407e1f
armyworm <baseballki66@gmail.com>
2015-10-30 14:03:50 +0800

    five

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,13 @@
 class EventsController < ApplicationController
 	def index
   	@events = Event.page(params[:page]).per(5)
+
+  	respond_to do |format|
+    format.html # index.html.erb
+    format.xml { render :xml => @events.to_xml }
+    format.json { render :json => @events.to_json }
+    format.atom { @feed_title = "My event list" } # index.atom.builder
+  	end
 	end
 
 	def new
@@ -17,6 +24,11 @@ class EventsController < ApplicationController
 
 	def show
   		@event = Event.find(params[:id])
+  		respond_to do |format|
+    	format.html { @page_title = @event.name } # show.html.erb
+    	format.xml # show.xml.builder
+    	format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+  	end
 	end
 
 	def edit
